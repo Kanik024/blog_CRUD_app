db91cbddf5102b2d9d9dfc289bf07e89783e7057
Kanik Sharma <kaniksharma8949@gmail.com>
2024-05-17 23:04:41 +0530

    updated models file

--- app/models.py
+++ app/models.py
@@ -1,15 +1,39 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+from bson import ObjectId
 
-class Post(BaseModel):
+class PyObjectId(ObjectId):
+    @classmethod
+    def _get_validators_(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid object ID")
+        return ObjectId(v)
+
+    @classmethod
+    def _modify_schema_(cls, field_schema):
+        field_schema.update(type="string")
+
+class BlogPost(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     title: str
     content: str
     author: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: Optional[datetime]
 
 class Comment(BaseModel):
-    text: str
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    post_id: PyObjectId
+    content: str
     author: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
-class User(BaseModel):
-    username: str
-    email: str
+class Like(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    post_id: PyObjectId
+    user: str
